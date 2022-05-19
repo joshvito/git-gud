fd73888e9908372bab33e29361c630254ef5d8fb
David Meyers <dmeyers@anthology.com>
2022-05-19 14:27:00 -0400

    Makes PR current awesome

--- pr-current.sh
+++ pr-current.sh
@@ -1,10 +1,41 @@
 #!/bin/bash
 rep=$(basename `git rev-parse --show-toplevel`)
 bn=$(git branch --show-current)
-r=''
-read -p "PR Title: " tit
-read -p "PR Description: " desc
-read -p "Work Item Number(s): " wi
+tit=''
+desc=''
+wi=''
 tb=$(git rev-parse --abbrev-ref origin/HEAD | cut -c8-)
+OPTIND=1
 
-az repos pr create --detect --auto-complete true --delete-source-branch true --description "$desc" --repository "$rep" --source-branch "$bn" --squash false --target-branch "$tb" --title "$tit" --output table --work-items wi
+while getopts ":t:d:n:" opt 
+do
+  case "$opt" in
+    t) tit="$OPTARG";;
+    d) desc="$OPTARG";;
+	n) wi="$OPTARG";;
+    \?) echo "Invalid option: -$OPTARG" >&2
+        return 1;;
+  esac
+done
+
+if [ -z "$tit" ]
+then
+	read -p "PR Title: " tit
+fi
+
+if [ -z "$desc" ]
+then
+	read -p "PR Description: " desc
+fi
+
+if [ -z "$wi" ]
+then
+	read -p "Work Item Number(s): " wi
+fi
+
+if [ -z "$desc" ]
+then
+	desc=$tit
+fi
+
+az repos pr create --detect --auto-complete true --delete-source-branch true --description "$desc" --repository "$rep" --source-branch "$bn" --squash false --target-branch "$tb" --title "$tit" --output table --work-items "$wi"
