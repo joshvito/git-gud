059c093ea598f6c44b9dab32cd5c8a7343be2d8f
David Meyers <dmeyers@anthology.com>
2024-09-04 14:16:33 -0400

    Updates PR current

--- pr-current.sh
+++ pr-current.sh
@@ -4,38 +4,95 @@ bn=$(git branch --show-current)
 tit=''
 desc=''
 wi=''
+ac=false
+optMode=false
 tb=$(git rev-parse --abbrev-ref origin/HEAD | cut -c8-)
 OPTIND=1
 
-while getopts ":t:d:n:" opt 
+############################################################
+# Help                                                     #
+############################################################
+Help()
+{
+   # Display Help
+   echo "Makes a pull request in Azure DevOps"
+   echo
+   echo "Syntax: prcurrent [-t|d|n|c|h]"
+   echo "options:"
+   echo "t  <Title>             Set the Pull Request title."
+   echo "d  <Description>       Set the Pull Request description."
+   echo "n  <DevOps Ticket #>   Set the ticket number."
+   echo "c                      Set the Pull Request to use Auto Complete"
+   echo "h                      Show this help message."
+   echo
+}
+
+############################################################
+# Set PR Vars                                              #
+############################################################
+SetVars()
+{
+  if (! $optMode) && [ -z "$tit" ]
+  then
+    read -p "PR Title: " tit
+  fi
+
+  if (! $optMode) && [ -z "$desc" ]
+  then
+    read -p "PR Description: " desc
+  fi
+
+  if (! $optMode) && [ -z "$wi" ]
+  then
+    read -p "Work Item Number(s): " wi
+  fi
+}
+
+while getopts ":ht:d:n:c" opt 
 do
+  optMode=true;
   case "$opt" in
+    h) Help 
+      return 1;;
     t) tit="$OPTARG";;
     d) desc="$OPTARG";;
     n) wi="$OPTARG";;
+    c) ac=true;;
     \?) echo "Invalid option: -$OPTARG" >&2
         return 1;;
   esac
 done
 
-if [ -z "$tit" ]
-then
-	read -p "PR Title: " tit
-fi
+SetVars
 
-if [ -z "$desc" ]
-then
-	read -p "PR Description: " desc
-fi
-
-if [ -z "$wi" ]
-then
-	read -p "Work Item Number(s): " wi
-fi
+while [ -z "$tit" ]
+do
+  optMode=false;
+  SetVars
+done
 
 if [ -z "$desc" ]
 then
 	desc=$tit
+fi
+
+echo "Checking account state..."
+az account show  >/dev/null 2>&1
+status=$?
+
+if [ "$status" != "0" ]
+then
+    echo "Logging into Azure..."
+    az login --use-device-code --tenant 809fd6c8-b876-47a9-abe2-8be2888f4a55
+fi
+
+az account show  >/dev/null 2>&1
+status=$?
+
+if [ "$status" != "0" ]
+then
+    echo 'Unable to login to Azure';
+    return;
 fi
 
 if type jq &>/dev/null; then
@@ -45,7 +102,7 @@ else
     exit 0;
 fi
 
-pull_request=$(az repos pr create --detect --auto-complete true --delete-source-branch true --description "$desc" --repository "$rep" --source-branch "$bn" --squash true --target-branch "$tb" --title "$tit" --output json --work-items "$wi")
+pull_request=$(az repos pr create --detect --auto-complete $ac --delete-source-branch true --description "$desc" --repository "$rep" --source-branch "$bn" --squash true --target-branch "$tb" --title "$tit" --output json --work-items "$wi")
 
 pull_request_id=$(echo $pull_request | jq -r '.pullRequestId');
 url=$(echo $pull_request | jq -r '.repository.webUrl');
