b1bea66916e53abaaf803d9a3a59f8aefec76df8
David Meyers <dmeyers@anthology.com>
2026-07-20 15:13:19 -0400

    fix merge issues

--- pr-current.sh
+++ pr-current.sh
@@ -6,10 +6,67 @@ tit=''
 desc=''
 wi=''
 useAc=''
-ac=true
-draft=false
-optMode=false
+ac=1        # 1 = true (auto-complete), 0 = false
+draft=0     # 1 = true, 0 = false
+optMode=0   # 1 = true, 0 = false
+gitEditor=$(git config core.editor)
+
 tb=$(git rev-parse --abbrev-ref origin/HEAD | cut -c8-)
+<<<<<<< HEAD
+
+############################################################
+# Help                                                     #
+############################################################
+Help() {
+  cat << EOF
+Makes a pull request in Azure DevOps
+
+Syntax: prcurrent [-t|d|n|r|m|h]
+
+Options:
+  -t <Title>             Set the Pull Request title.
+  -d <Description>       Set the Pull Request description.
+  -n <DevOps Ticket #>   Set the ticket number.
+  -r                     Set the Pull Request to draft mode.
+  -m                     Use manual complete mode (disable auto-complete).
+  -h                     Show this help message.
+
+EOF
+}
+
+############################################################
+# Set PR Vars                                              #
+############################################################
+SetVars() {
+  if [ -z "$tit" ]; then
+    read -rp "PR Title: " tit
+  fi
+
+  if [ "$optMode" -eq 0 ] && [ -z "$desc" ]; then
+    # Open editor to enter description
+    tmpfile=$(mktemp /tmp/prdesc.XXXXXX)
+    ${gitEditor:-vim} "$tmpfile"
+    desc=$(<"$tmpfile")
+    rm -f "$tmpfile"
+  fi
+
+  if [ -z "$wi" ]; then
+    read -rp "Work Item Number(s): " wi
+  fi
+
+  if [ "$optMode" -eq 0 ] && [ -z "$useAc" ]; then
+    read -rp "Use Auto Complete [Y]: " useAc
+    useAc=${useAc:-Y}
+    if [[ "$useAc" != "Y" && "$useAc" != "y" ]]; then
+      ac=0
+    fi
+  fi
+}
+############################################################
+
+OPTIND=1
+=======
+>>>>>>> 1ca3623 (Updates PR Current to use git configured editor for descriptions for PRs + fixes az account check)
 
 ############################################################
 # Help                                                     #
@@ -63,97 +120,36 @@ SetVars() {
 
 OPTIND=1
 
-############################################################
-# Help                                                     #
-############################################################
-Help()
-{
-   # Display Help
-   echo "Makes a pull request in Azure DevOps"
-   echo
-   echo "Syntax: prcurrent [-t|d|n|r|m|h]"
-   echo "options:"
-   echo "t  <Title>             Set the Pull Request title."
-   echo "d  <Description>       Set the Pull Request description."
-   echo "n  <DevOps Ticket #>   Set the ticket number."
-   echo "r                      Set the Pull Request to use draft mode (Rough Draft)."
-   echo "m                      Set the Pull Request to use manual Complete mode."
-   echo "h                      Show this help message."
-   echo
-}
-
-############################################################
-# Set PR Vars                                              #
-############################################################
-SetVars()
-{
-  if [ -z "$tit" ]
-  then
-    read -p "PR Title: " tit
-  fi
-
-  if ( ! $optMode ) && [ -z "$desc" ]
-  then
-    read -p "PR Description: " desc
-  fi
-
-  if [ -z "$wi" ]
-  then
-    read -p "Work Item Number(s): " wi
-  fi
-
-  if ( ! $optMode ) && [ -z "$useAc" ]
-  then
-    read -p "Use Auto Complete [Y]: " useAc
-    useAc=${useAc:-Y}
-    
-    if [ "$useAc" != "Y" ] && [ "$useAc" != "y" ]
-    then
-      ac=false
-    fi
-  fi
-}
-
-while getopts ":ht:d:n:m:r" opt 
+# Parse short options with getopts
+while getopts ":ht:d:n:mr" opt 
 do
-  optMode=true;
+  optMode=1;
   case "$opt" in
-    h) Help 
-      return 1;;
+    h) Help; return 1;;
     t) tit="$OPTARG";;
     d) desc="$OPTARG";;
     n) wi="$OPTARG";;
-    m) ac=false;;
-    r) draft=true;;
-    \?) echo "Invalid option: -$OPTARG" >&2
-        return 1;;
+    m) ac=0;;
+    r) draft=1;;
+    \?) echo "Invalid option: -$OPTARG" >&2; return 1;;
   esac
 done
 
 SetVars
 
-if [ -z "$desc" ]
-then
-	desc=$tit
+if [ -z "$desc" ]; then
+  desc="$tit"
 fi
 
 echo "Checking account state..."
-az account show  >/dev/null 2>&1
-status=$?
-
-if [ "$status" != "0" ]
-then
-    echo "Logging into Azure..."
-    az login --use-device-code --tenant 809fd6c8-b876-47a9-abe2-8be2888f4a55
+if ! az ad signed-in-user show >/dev/null 2>&1; then
+  echo "Logging into Azure..."
+  az login --use-device-code --tenant 809fd6c8-b876-47a9-abe2-8be2888f4a55
 fi
 
-az account show  >/dev/null 2>&1
-status=$?
-
-if [ "$status" != "0" ]
-then
-    echo 'Unable to login to Azure';
-    return;
+if ! az ad signed-in-user show >/dev/null 2>&1; then
+  echo "Unable to login to Azure"
+  return 1
 fi
 
 if type jq &>/dev/null; then
@@ -163,7 +159,10 @@ else
     return 1;
 fi
 
-pull_request=$(az repos pr create --detect --auto-complete $ac --draft $draft --delete-source-branch true --description "$desc" --repository "$rep" --source-branch "$bn" --squash true --target-branch "$tb" --title "$tit" --output json --work-items "$wi")
+ac_str="true"
+if [ "$ac" -eq 0 ]; then
+  ac_str="false"
+fi
 
 draft_str="false"
 if [ "$draft" -eq 1 ]; then
@@ -220,4 +219,8 @@ echo '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⣶⣤⣀⡉⠙⠓�
 echo '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠻⠿⣶⣶⣦⣤⣤⣤⣤⣤⣤⣤⣴⣶⣶⡿⠿⠛⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀'
 echo '⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀'
 
+<<<<<<< HEAD
 echo "$joined"
+=======
+echo "$joined"
+>>>>>>> 1ca3623 (Updates PR Current to use git configured editor for descriptions for PRs + fixes az account check)
